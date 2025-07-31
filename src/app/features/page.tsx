66b31e05c352f1a07091d55af9b7234835c19b9f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-31 15:52:05 +0500

    created features section

---
+++ src/app/features/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FeaturesPage = () => {
+  return (
+     <div className='w-full h-auto flex justify-center items-center lg:py-30 md:py-15 sm:py-10 py-5'>
+                <div className='w-full flex-col gap-10 h-full flex justify-center items-center'>
+                    <h2 className='text-3xl font-extrabold text-center'>How I Can Help</h2>
+    
+                    <div className='w-full h-auto flex flex-col'>
+                    
+                    </div>
+                </div>
+            </div>
+  )
+}
+
+export default FeaturesPage
--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const HelpPage = () => {
     return (
-        <div className='w-full h-auto flex justify-center items-center'>
+        <div className='w-full h-auto flex justify-center items-center lg:py-30 md:py-15 sm:py-10 py-5'>
             <div className='xl:w-[60%] lg:w-[75%] md:w-[90%] w-full flex-col gap-10 h-full flex justify-center items-center'>
                 <h2 className='text-3xl font-extrabold text-center'>How I Can Help</h2>
 
