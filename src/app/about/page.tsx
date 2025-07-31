607e874bb2d60f851709be059e65be972f6d6c92
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-31 14:59:51 +0500

    creating about pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div className='w-full h-auto flex justify-center items-center lg:py-30 md:py-15 sm:py-10 py-5'>
+            <div className='lg:w-[85%] md:w-[95%] w-full h-full flex justify-center items-center'>
+                <div className='xl:w-[856px] lg:w-[800px] md:w-[750px] h-[362px] flex justify-center items-center p-6 rounded-[20px] bg-[#F5F5F5]'>
+                    <div className='w-full h-full flex gap-6'>
+<div className='w-[40%] h-full'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutPage
