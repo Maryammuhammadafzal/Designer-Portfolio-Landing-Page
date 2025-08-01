25c75ee294775453b86e8285cdac3a4c93dbd5a4
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-01 16:08:21 +0500

    craeting crousel

--- src/components/FeatureCarousel.tsx
+++ src/components/FeatureCarousel.tsx
@@ -1,54 +1,15 @@
 "use client"
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
+import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 import { Button } from './ui/button';
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
-const Slider = dynamic(() => import('react-slick'), { ssr: false });
+// const Slider = dynamic(() => import('react-slick'), { ssr: false });
 
 const FeatureCarousel = () => {
-
-   const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 1279,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 640,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
 
     const carousel_data = [
         {
@@ -79,41 +40,27 @@ const FeatureCarousel = () => {
             bgColour: '#D2C7F266',
             colour: '#1301BD',
         },
-        {
-            title: 'Bloom App',
-            subTitle: 'Wellness App UI/UX',
-            description: 'A calm and intuitive mobile app that helps women track habits, mood, and self-care goals.',
-            bgColour: '#D2C7F266',
-            colour: '#1301BD',
-        },
-        {
-            title: 'Bloom App',
-            subTitle: 'Wellness App UI/UX',
-            description: 'A calm and intuitive mobile app that helps women track habits, mood, and self-care goals.',
-            bgColour: '#D2C7F266',
-            colour: '#1301BD',
-        },
     ]
     return (
-        <div className='w-full h-auto flex justify-center items-center'>
-            <Slider {...settings} className='w-full max-h-[450px] flex'>
+        <Carousel className='w-full max-h-[450px] flex'>
+            <CarouselContent className="w-full">
+                {/* Card 1 */}
                 {
                     carousel_data.map((data, index) => (
-                        <div key={index} className={`w-[700px] ml-5 h-[360px] bg-[${data.bgColour}] flex-row flex rounded-[20px]`}>
+                        <CarouselItem key={index} className='max-w-[700px] max-h-[360px] min-w-[700px] ml-4 min-h-[360px] bg-[#D2C7F266] flex-row flex rounded-[20px]'>
 
-                            <div className='w-full h-full flex flex-row'>
+                            <div className='w-full h-full flex justify-between flex-row'>
                                 {/* Left side */}
                                 <div className='w-[45%] h-full flex justify-center items-center'>
                                     <div className='w-full h-full flex flex-col gap-4 justify-center'>
                                         <div className='w-full flex flex-col'>
-                                            <h2 className={`text-[40px] font-extrabold leading-12 text-[${data.colour}] `}>{data.title}</h2>
-                                            <p className='text-sm'>{data.subTitle}</p>
+                                            <h2 className='text-[40px] font-extrabold leading-12 text-[#1301BD] '>{data.title}</h2>
+                                            <p className='text-sm'>Wellness App UI/UX</p>
                                         </div>
-                                        <p className='text-foreground/40 pt-3 sm:text-base text-sm capitalize'>
-                                            {data.description}
+                                        <p className='text-foreground/40 pt-3 sm:text-base text-sm capitalize'>A calm and intuitive mobile app that helps women track habits, mood, and self-care goals.
                                         </p>
                                         <div>
-                                            <Button className={`bg-[${data.colour}] mt-6 rounded-4xl px-7 py-5 flex gap-3 text-white text-center`}>
+                                            <Button className='bg-[#1301BD] mt-6 rounded-4xl px-7 py-5 flex gap-3 text-white text-center'>
                                                 Say live <Image src='/images/arrow-icon.png' alt='image' height={26} width={10} className='w-[26px]' />
                                             </Button>
                                         </div>
@@ -127,11 +74,12 @@ const FeatureCarousel = () => {
                                 </div>
                             </div>
 
-                        </div>
+                        </CarouselItem>
+
                     ))
                 }
-            </Slider>
-        </div>
+            </CarouselContent>
+        </Carousel>
     )
 }
 
