a0f8356cc9e9d546f5470a692aa58b6d96e3dd22
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-01 15:33:12 +0500

    creating footer section responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
         <div className='w-full flex justify-center items-center lg:py-30 md:py-15 sm:py-10 py-5 max-sm:px-3'>
             <div className='lg:w-[85%] md:w-[95%] w-full h-full flex flex-col justify-center items-center text-center'>
                 <p className='text-foreground/40 pt-3 sm:text-base text-sm'>Designed with 💖 and clean pixels.</p>
-                <h2 className='lg:text-[40px] font-extrabold text-center'>© 2025 Aria Bennett</h2>
-                <div className='flex gap-1 h-auto w-auto text-foreground/40'><Link href='/'>Instagram</Link> . <Link href='/'>Dribble</Link> . <Link href='/'>Behance</Link> . <Link href='/'>LinkedIn</Link> .</div>
+                <h2 className='md:text-[40px] xs:text-4xl 2xs:text-3xl text-2xl py-2 font-extrabold text-center'>© 2025 Aria Bennett</h2>
+                <div className='flex gap-1 h-auto w-auto text-foreground/40 xs:text-base'><Link href='/'>Instagram</Link> . <Link href='/'>Dribble</Link> . <Link href='/'>Behance</Link> . <Link href='/'>LinkedIn</Link> .</div>
             </div>
         </div>
     )
