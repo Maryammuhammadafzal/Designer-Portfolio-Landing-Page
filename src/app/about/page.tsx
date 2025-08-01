e90c437a26410cc54af0036045b99a15112ffb1c
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-01 15:21:08 +0500

    creating about section responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,15 +5,15 @@ const AboutPage = () => {
     return (
         <div className='w-full h-auto flex justify-center items-center lg:py-30 md:py-15 sm:py-10 py-5 max-sm:px-3'>
             <div className='lg:w-[85%] md:w-[95%] w-full h-full flex justify-center items-center'>
-                <div className='xl:w-[856px] lg:w-[800px] md:w-[750px] h-[362px] flex justify-center items-center md:p-6 sm:p-4 p-2 rounded-[20px] bg-[#F5F5F5]'>
-                    <div className='w-full h-full flex gap-6'>
+                <div className='xl:w-[856px] lg:w-[800px] md:w-[750px] sm:w-full w-[350px] sm:h-[362px] flex justify-center items-center md:p-6 p-4 rounded-[20px] bg-[#F5F5F5]'>
+                    <div className='w-full h-full sm:flex-row flex-col flex gap-6'>
                         {/* Left side */}
-                        <div className='w-[40%] h-full flex justify-center items-center'>
+                        <div className='md:w-[40%] w-full h-full flex justify-center items-center'>
                             <div className='w-full h-full flex flex-col gap-3 justify-center items-center'>
                                 {/* Bussiness Card */}
                                 <div className='w-full h-[130px] relative flex justify-end items-end p-3 rounded-[20px] bg-white'>
                                     <h2 className='lg:text-3xl md:text-2xl text-xl font-extrabold lg:max-w-[130px] md:max-w-[110px] max-w-[90px]'>Aria Bennett</h2>
-                                    <div className='md:w-[150px] md:h-[150px] w-[120px] h-[120px] absolute left-2 -top-10'>
+                                    <div className='md:w-[150px] md:h-[150px] w-[120px] h-[120px] absolute md:left-2 left-0 md:-top-10 top-0'>
                                         <Image src='/images/profile-image.png' alt='image' height={150} width={150} className='w-full h-full' />
                                     </div>
                                 </div>
@@ -36,7 +36,7 @@ const AboutPage = () => {
                             </div>
                         </div>
                         {/* Right side */}
-                        <div className='w-[60%] h-full flex justify-center items-center'>
+                        <div className='md:w-[60%] w-full h-full flex justify-center items-center'>
                             <div className='w-full h-full'>
                                 <h2 className='lg:text-[40px] md:text-4xl text-3xl font-extrabold '>More Than Just a <br /> Designer</h2>
                                 <p className='text-foreground/40 pt-3 sm:text-base text-sm'>Hi, I’m <span className='font-bold'>Aria Bennett</span> — a designer based in Austin, TX with a passion for meaningful visuals and clean interfaces. I blend strategy with storytelling to build elegant, intuitive, and inclusive designs.
