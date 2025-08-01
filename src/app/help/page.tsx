69a56ba163cf81b00b05c71505e09941d7f8e17c
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-01 15:27:00 +0500

    creating help section responsive

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -10,59 +10,59 @@ const HelpPage = () => {
                 <div className='w-full h-auto flex flex-col'>
                     {/* 1 */}
                     <div className='flex w-full h-auto border-b border-primary/20'>
-                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start h-[124px] p-3'>
-                            <p className='md:text-2xl text-xl w-full h-full flex items-end pb-3 text-primary/70'>
+                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-2xl sm:text-xl xs:text-lg text-base w-full h-full flex items-end pb-3 text-primary/70'>
                                 🖥️ <br /> UI/UX Design
                             </p>
                         </div>
-                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] p-3'>
-                            <p className='md:text-lg text-base text-primary/70 max-w-xs'>
+                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-lg sm:text-base xs:text-sm text-xs text-primary/70 max-w-xs'>
                                 Beautiful interfaces that feel effortless to use.
                             </p>
                         </div>
-                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] p-3'>
+                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] sm:p-3 p-1'>
                             <p className='text-4xl text-primary/70 flex gap-3 justify-center items-center w-full h-full'>
-                                <Image src='/images/figma-icon.png' alt='image' height={40} width={40} />
-                                <Image src='/images/xd-icon.png' alt='image' height={40} width={40} />
+                                <Image src='/images/figma-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
+                                <Image src='/images/xd-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
                             </p>
                         </div>
                     </div>
                     {/* 2 */}
                     <div className='flex w-full h-auto'>
-                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start items-center h-[124px] p-3'>
-                            <p className='md:text-2xl text-xl w-full h-full flex items-end pb-3 text-primary/70'>
+                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start items-center h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-2xl sm:text-xl xs:text-lg text-base w-full h-full flex items-end pb-3 text-primary/70'>
                                 🎨 <br />
                                 Brand Identity
                             </p>
                         </div>
-                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] p-3'>
-                            <p className='md:text-lg text-base text-primary/70 max-w-xs'>
+                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-lg sm:text-base xs:text-sm text-xs text-primary/70 max-w-xs'>
                                 From logos to brand guidelines — your story, uniquely told.
                             </p>
                         </div>
-                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] p-3'>
+                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] sm:p-3 p-1'>
                             <p className='text-4xl text-primary/70 flex gap-3 justify-center items-center w-full h-full'>
-                                <Image src='/images/adobe-icon.png' alt='image' height={40} width={40} />
-                                <Image src='/images/photoshop-icon.png' alt='image' height={40} width={40} />
+                                <Image src='/images/adobe-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
+                                <Image src='/images/photoshop-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
                             </p>
                         </div>
                     </div>
                     <div className='flex w-full h-auto border-t border-primary/20'>
-                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start items-center h-[124px] p-3'>
-                            <p className='md:text-2xl text-xl w-full h-full flex items-end pb-3 text-primary/70'>
+                        <div className='w-[30%] border-r border-primary/20 flex flex-col gap-3 justify-start items-center h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-2xl sm:text-xl xs:text-lg text-base w-full h-full flex items-end pb-3 text-primary/70'>
                                 📱 <br />
                                 Mobile Design
                             </p>
                         </div>
-                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] p-3'>
-                            <p className='md:text-lg text-base text-primary/70 max-w-xs'>
+                        <div className='w-[40%] flex flex-col gap-3 justify-center text-center items-center h-[124px] sm:p-3 p-1'>
+                            <p className='md:text-lg sm:text-base xs:text-sm text-xs text-primary/70 max-w-xs'>
                                 Intuitive mobile-first experiences that engage and convert.
                             </p>
                         </div>
-                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] p-3'>
+                        <div className='w-[30%] border-l border-primary/20 flex flex-col gap-3 justify-end items-center h-[124px] sm:p-3 p-1'>
                             <p className='text-4xl text-primary/70 flex gap-3 justify-center items-center w-full h-full'>
-                                <Image src='/images/figma-icon.png' alt='image' height={40} width={40} />
-                                <Image src='/images/xd-icon.png' alt='image' height={40} width={40} />
+                                <Image src='/images/figma-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
+                                <Image src='/images/xd-icon.png' alt='image' height={40} width={40} className='xs:w-[40px] xs:h-[40px] w-[30px] h-[30px]' />
                             </p>
                         </div>
                     </div>
